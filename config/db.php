d3fed7697818f4578eaed25b6fe62a1c65e35c3c
FilipeCampos2424 <filipegames2343@gmail.com>
2026-04-27 20:10:22 -0300

    Config/db.php preenchido

---
+++ config/db.php
@@ -0,0 +1,18 @@
+<?php 
+
+function db(): PDO {
+    $host = '127.0.0.1';
+    $db_name = 'crud_php';
+    $user = 'root';
+    $pass = '';
+
+    $dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
+    $pdo = new PDO($dsn, $user, $pass, [
+        PDO::ATTTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES => false,
+    ]);
+
+    return $pdo;
+}
+?>
